a17a5894deddd91028fc718bf4246d6b1d4242be
Brandon K Bae <bkunjun@gmail.com>
2021-03-10 18:14:40 -0500

    partID Method

--- hocrExtraction/articleFromTitle.py
+++ hocrExtraction/articleFromTitle.py
@@ -1,7 +1,20 @@
 import pandas as pd
 import os
 
-def makeTxtFiles(path):
-    articleFolder = './test/' +
-    if not os.path.exists(newpath):
-        os.makedirs(newpath)
+def findTitles(path):
+    partID = []
+    id = 0
+    df = pd.read_csv(path)
+    onTitle = False
+    isTitleList = df['isTitle'].tolist()
+    for x in isTitleList:
+        if not(onTitle) and x == True:
+            id = id+1
+            onTitle = True
+        elif onTitle and x == False:
+            onTitle = False
+        partID.append(id)
+    df['PartID'] = partID
+    df.to_csv('./test/testPartID.csv', index = False)
+if __name__ == '__main__':
+    findTitles('./test/testTitle.csv')
