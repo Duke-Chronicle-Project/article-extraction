e730cbf57ce8f542c17239477ae4700e8f72c1ef
neel216 <neelr216@gmail.com>
2021-03-13 01:30:21 -0500

    added font size extraction

--- hocrExtraction/hocrExtraction.py
+++ hocrExtraction/hocrExtraction.py
@@ -1,6 +1,6 @@
 '''
 hOCR extraction using the hOCR standards defined here: http://kba.cloud/hocr-spec/1.2/
-sampleInput.hocr generated using tesseract 4.1.1 command `tesseract alice_1.png sampleInput hocr`
+sampleInput.hocr generated using tesseract 4.1.1 command `tesseract alice_1.png sampleInput hocr -c hocr_font_info=1`
 '''
 
 from lxml import html
@@ -17,7 +17,8 @@ def parseAttrib(attributes):
         wordBbox = [int(i) for i in wordProps[0].split()[1:]]   # get word bounding box
         wordConf = int(wordProps[1].split()[1])                 # get word confidence
         wordNum = int(attributes['id'].split('_')[-1])          # get word number
-        return wordBbox, wordConf, wordNum
+        fontSize = int(wordProps[2].split()[1])                 # get word font size
+        return wordBbox, wordConf, wordNum, fontSize
 
     elif attributes['class'] == 'ocr_line':
         lineProps = attributes['title'].split('; ')
@@ -65,15 +66,15 @@ def parseHOCR(path, save=False):
                     lineBbox, lineHeight, lineNum = parseAttrib(line.attrib)
 
                     for word in line.xpath("./*[@class='ocrx_word']"):
-                        wordBbox, wordConf, wordNum = parseAttrib(word.attrib)
+                        wordBbox, wordConf, wordNum, fontSize = parseAttrib(word.attrib)
 
-                        final.append((word.text, wordConf, wordNum, wordBbox[0], wordBbox[1], wordBbox[2], wordBbox[3],
+                        final.append((word.text, wordConf, wordNum, fontSize, wordBbox[0], wordBbox[1], wordBbox[2], wordBbox[3],
                                       lineNum, lineHeight, lineBbox[0], lineBbox[1], lineBbox[2], lineBbox[3],
                                       parNum, parBbox[0], parBbox[1], parBbox[2], parBbox[3],
                                       colAreaNum, colAreaBbox[0], colAreaBbox[1], colAreaBbox[2], colAreaBbox[3],
                                       pageNum, pageBbox[0], pageBbox[1], pageBbox[2], pageBbox[3], pageImage))
 
-    df = pd.DataFrame(final, columns=['word', 'conf', 'wordNum', 'wordX0', 'wordY0', 'wordX1', 'wordY1',
+    df = pd.DataFrame(final, columns=['word', 'conf', 'wordNum', 'fontSize', 'wordX0', 'wordY0', 'wordX1', 'wordY1',
                                       'lineNum', 'lineHeight', 'lineX0', 'lineY0', 'lineX1', 'lineY1',
                                       'parNum', 'parX0', 'parY0', 'parX1', 'parY1',
                                       'colAreaNum', 'colAreaX0', 'colAreaY0', 'colAreaX1', 'colAreaY1',
